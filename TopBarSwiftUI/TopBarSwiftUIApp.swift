b5157c076411ca5ade36e2662f7af12ac4975ca8
Ahmet <ahmetbostancikli@gmail.com>
2025-12-03 11:35:22 +0300

    Initial Commit

---
+++ TopBarSwiftUI/TopBarSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TopBarSwiftUIApp.swift
+//  TopBarSwiftUI
+//
+//  Created by MrBostan on 3.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct TopBarSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
